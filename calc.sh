e6a0746b384f4cc8791481a0711b20b36fcb14ad
Igor <drkierkegor@gmail.com>
2018-03-20 12:26:09 +0300

    calc : +cond for case with division to zero and exp for zero nad for negative exp

--- calc.sh
+++ calc.sh
@@ -22,23 +22,36 @@ calc ()
       echo $res
       ;;
     "/")
-      let "res = $a / $c"
-      echo $res
+      if [[ $c -eq 0 ]]
+      then
+        echo "You can't divide to zero"
+      else
+        let "res = $a / $c"
+        echo $res
+      fi
       ;;
     "%")
       let "res = $a % $c"
       echo $res
       ;;
     "**")
-      let "res = $a ** $c"
-      echo $res
+      if [[ $a -ne 0 && $c -eq 0 ]]
+      then
+        echo 1
+      elif [[ $c -lt 0 ]]
+      then
+        echo "This calc not allows use negative exponents."
+      else
+        let "res = $a ** $c"
+        echo $res
+      fi
       ;;
     *)
-      echo "error"
+      echo "An error occurred. Please, check source conditions and try again."
       break
     esac
   else
-    echo "error"
+    echo "An error occurred. Please, check source conditions and try again."
     exit
   fi
 }
